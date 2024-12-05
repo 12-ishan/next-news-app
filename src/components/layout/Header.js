0df6cf75c4e8947d1f4be04163c5b876c169d1f0
Codeavoid <rajatvarshney2009@gmail.com>
2024-12-05 22:19:43 +0530

    ham burger

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,18 +1,20 @@
 'use client';
 import Link from 'next/link';
 import '../../../styles/css/style.css';
-import '../../../styles/css/bootstrap/bootstrap-grid.css';
-import '../../../styles/css/bootstrap/bootstrap-reboot.css';
-import '../../../styles/css/aos.css';
-import '../../../styles/css/bootstrap.min.css';
-import '../../../styles/css/magnific-popup.css';
-import '../../../styles/css/owl.theme.default.min.css';
-import '../../../styles/scss/style.scss';
-import '../../../styles/scss/bootstrap/bootstrap.scss';
-import '../../../public/assets/fonts/icomoon/style.css';
-import '../../../styles/css/owl.carousel.min.css';
+import styles from '../../../styles/Header.module.css';
+
+//import '../../../styles/css/bootstrap/bootstrap-grid.css';
+//import '../../../styles/css/bootstrap/bootstrap-reboot.css';
+//import '../../../styles/css/aos.css';
+//import '../../../styles/css/bootstrap.min.css';
+// /import '../../../styles/css/magnific-popup.css';
+//import '../../../styles/css/owl.theme.default.min.css';
+// import '../../../styles/scss/style.scss';
+// import '../../../styles/scss/bootstrap/bootstrap.scss';
+// import '../../../public/assets/fonts/icomoon/style.css';
+// import '../../../styles/css/owl.carousel.min.css';
 import NewsCategories from '../NewsCategories';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { useSelector, useDispatch } from 'react-redux';
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
@@ -28,6 +30,12 @@ function Header() {
   const [searchQuery, setSearchQuery] = useState('');
   const [isLoading, setIsLoading] = useState(true);
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   useEffect(() => {
     dispatch(websiteLogo()).finally(() => {
       setIsLoading(false);
@@ -42,64 +50,72 @@ function Header() {
   };
 
   return (
-    <header className="site-navbar" role="banner">
-      <div className="site-navbar-top">
+    <header className={`${styles.navbar} site-navbar`} role="banner">
+      <div>
         <div className="container">
-          <div className="row align-items-center flex-row-reverse">
-            <div className="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-left">
-              <div>
-                <Link href="/" className="js-logo-clone">
-                  {isLoading ? (
-                    <Skeleton width={100} height={40} />
-                  ) : (
-                    <Image
-                      className="img-fluid"
-                      src={logo.image}
-                      alt="logo"
-                      width={100}
-                      height={100}
-                    />
-                  )}
-                </Link>
+
+          <div className={`${styles.row}`}>
+
+            <div className={styles.menuContainer}>
+              <div className={styles.menuIcon} onClick={toggleMenu}>
+                <span className={`${styles.bar} ${isOpen ? styles.bar1 : ""}`}></span>
+                <span className={`${styles.bar} ${isOpen ? styles.bar2 : ""}`}></span>
+                <span className={`${styles.bar} ${isOpen ? styles.bar3 : ""}`}></span>
               </div>
+              <nav className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
+                <ul>
+                  <li>
+                    <Link href="/">Home</Link>
+                  </li>
+                  <NewsCategories />
+                  <li>
+                    <Link href="/contact">Contact</Link>
+                  </li>
+                </ul>
+              </nav>
             </div>
 
-            <div className="col-6 col-md-4 order-2 order-md-1 site-search-icon text-left">
+            <div className={`${styles.navWidth} order-1`}>
+              <Link href="/" className="js-logo-clone">
+                {isLoading ? (
+                  <Skeleton width={150} height={60} />
+                ) : (
+                  <Image
+                    className="img-fluid"
+                    src={logo.image}
+                    alt="logo"
+                    width={150}
+                    height={60}
+                  />
+                )}
+              </Link>
+             
+            </div>
+
+            <div className={`${styles.searchBox} order-1`}>
               <form
                 onSubmit={handleSearch}
                 className="site-block-top-search d-flex align-items-center"
               >
-                <span className="icon icon-search2"></span>
                 <input
                   type="text"
-                  className="form-control border-0 mx-2"
+                  className={`${styles.searchInput} form-control`}
                   placeholder="Search"
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
                 />
-                <button type="submit" className="btn btn-outline-secondary">
+
+                <button type="submit" className="btn btn-dark">
                   Search
                 </button>
               </form>
-            </div>
-
-            <div className="col-6 col-md-4 order-3 order-md-3 text-right">
-              <div className="site-top-icons">
-                <ul>
-                  <li className="d-inline-block d-md-none ml-md-0">
-                    <a href="/" className="site-menu-toggle js-menu-toggle">
-                      <span className="icon-menu"></span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
             </div>
           </div>
         </div>
       </div>
 
       <nav
-        className="site-navigation text-right text-md-center"
+        className={`${styles.mainMenu} site-navigation text-right text-md-center`}
         role="navigation"
       >
         <div className="container">
